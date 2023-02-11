2435574e8a98031374b0812d7b462a5ed09fe20c
srinivas <srinu781997@gmail.com>
2023-02-11 15:31:15 +0000

    some changes are updated

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.post("/books/", async (request, response) => {
 
 //Update Book API
 
-app.put("/books/:bookId:", async (request, response) => {
+app.put("/books/:bookId/", async (request, response) => {
   const { bookId } = request.params;
   const bookDetails = request.body;
   const {
@@ -130,4 +130,28 @@ app.put("/books/:bookId:", async (request, response) => {
 
   await db.run(updateBookQuery);
   response.send("Book updated successfully!");
+});
+
+app.delete("/books/:bookId/", async (request, response) => {
+  const { bookId } = request.params;
+  const deleteBookQuery = `
+    DELETE FROM
+        book
+    WHERE
+        book_id = ${bookId};`;
+  await db.run(deleteBookQuery);
+  response.send("Book Deleted Successfully");
+});
+
+app.get("/authors/:authorId/books", async (request, response) => {
+  const { authorId } = request.params;
+  const getAuthorBooksQuery = `
+    SELECT
+    *
+    FROM
+        book
+    WHERE
+        author_id = ${authorId};`;
+  const booksArray = await db.all(getAuthorBooksQuery);
+  response.send(booksArray);
 });
